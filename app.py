79847b5f4c560181a294c466b344f4314892c9d5
unknown <s22595@pjwstk.edu.pl>
2024-06-19 13:38:57 +0200

    NewApp

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import LabelEncoder
 startTime = datetime.now()
 
 #Load the pre-trained model
-#win_model = pickle.load(open('./ml_models/win_pred.pkl','rb'))
+win_model = pickle.load(open('./ml_models/win_pred.pkl','rb'))
 
 st.set_page_config(page_title="PJATK Football")
 st.header("PJATK Sports Bet")
@@ -44,3 +44,45 @@ country = st.selectbox("Wybierz Kraj", np.append("", df['country'].unique()))
 city = st.selectbox("Wybierz Miasto", np.append("", countrycity_walidation.get(country, [""])))
 
 date = st.date_input("Wybierz Datę Meczu")
+
+def predict_result(home_team, away_team, neutral, tournament, city, country, date):
+    if home_team == away_team:
+        # Jeśli wybrane drużyny są takie same, zwróć remis
+        return 0, 1, 0
+
+    # Kodowanie wartości
+    home_team_encoded = team_encoder.transform([home_team])[0]
+    away_team_encoded = team_encoder.transform([away_team])[0]
+    tournament_encoded = tournament_encoder.transform([tournament])[0]
+    city_encoded = city_encoder.transform([city])[0]
+    country_encoded = countrycity_walidation.transform([country])[0]
+    
+    # Zakodowanie daty jako string
+    date_encoded = LabelEncoder().fit_transform([str(date)])[0]
+
+    # Utworzenie DataFrame z danymi wejściowymi
+    input_data = pd.DataFrame({
+        'date': [date_encoded],
+        'home_team': [home_team_encoded],
+        'away_team': [away_team_encoded],
+        'home_score': [1],  # Możesz dostosować zgodnie z wymaganiami modelu
+        'tournament': [tournament_encoded],
+        'city': [city_encoded],
+        'country': [country_encoded],
+        'neutral': [neutral]
+    })
+
+    # Ustawienie odpowiedniej kolejności kolumn
+    expected_columns = ['date', 'home_team', 'away_team', 'home_score', 'tournament', 'city', 'country', 'neutral']
+    input_data = input_data[expected_columns]
+
+    # Wyświetlenie danych wejściowych dla celów diagnostycznych
+    st.write("Dane wejściowe do predykcji:", input_data)
+
+    # Przewidywanie wyników
+    win_prob = win_model.predict_proba(input_data)[0][1]
+    
+
+    return win_prob
+
+predict = predict_result(home_team, away_team, neutral, tournament, city, country, date)
